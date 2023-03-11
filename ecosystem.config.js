bb75d9a48081b91c2be2e3c1a496159e58588766
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-11 17:20:12 +0600

    npvAdded

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'Nuxt3',
+      name: 'NuxtAppName',
+      port: '3000',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      instances: 'max',
+      script: './.output/server/index.mjs'
     }
   ]
 }
