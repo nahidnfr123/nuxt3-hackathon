8b72d15c00c5f87582f9375c52edde28bb1af069
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-11 17:36:21 +0600

    npvAdded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
   // },
   runtimeConfig: {
     public: {
-      siteName: 'Nuxt, Pinia, Laravel Starter',
+      siteName: 'Hackathon',
       apiBaseUrl: process.env.NUXT_API_BASE_URL || 'http://127.0.0.1:8088/api/'
     }
   }
